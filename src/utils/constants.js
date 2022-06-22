7dccf938d84af9d50121da49fee3b284c0da5fdf
VladiVK <kozobon@gmail.com>
2022-06-22 18:58:09 +0300

    chore: setup utils directory: contants.js , static.json

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+import { FaCalendarAlt, FaDoorOpen, FaUsers } from 'react-icons/fa';
+
+export const links = [
+  { id: 1, title: 'bookings', url: '/', icon: <FaCalendarAlt /> },
+  { id: 2, title: 'bookable', url: '/bookable', icon: <FaDoorOpen /> },
+  { id: 3, title: 'users', url: '/users', icon: <FaUsers /> },
+];
