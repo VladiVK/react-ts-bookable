d57f46a206fd7b7079849f54b7284d68c531166d
VladiVK <kozobon@gmail.com>
2022-06-23 21:36:18 +0300

    chore: add Sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import { FaTimes } from 'react-icons/fa';
 import UserPicker from '../userPicker';
 import { SidebarWrapper } from './style';
+import Logo from '../../assets/icons/main-logo.svg';
+import { links } from '../../utils/constants';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <SidebarWrapper>
-      <aside className='sidebar sidebar-show'>
-        <header className='sidebar__header'></header>
-        <ul className='sidebar__links'></ul>
-        <UserPicker />
+      <aside className='sidebar'>
+        <header className='sidebar__header'>
+          <img src={Logo} alt='bookable logo' className='sidebar__logo' />
+          <button type='button' className='sidebar__btn-close'>
+            <FaTimes />
+          </button>
+        </header>
+        <ul className='sidebar__links'>
+          {links.map(({ id, icon, title, url }) => (
+            <li key={id} className='sidebar__link'>
+              <NavLink to={url}>
+                {icon}
+                <span>{title}</span>
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <div className='sidebar__users'>
+          <UserPicker />
+        </div>
       </aside>
     </SidebarWrapper>
   );
--- src/components/sidebar/style.js
+++ src/components/sidebar/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const SidebarWrapper = styled.div`
-  background: var(--main-bg);
+  background: var(--clr-main-bg);
 
   .sidebar {
     position: fixed;
@@ -10,13 +10,94 @@ export const SidebarWrapper = styled.div`
     width: 100%;
     height: 100%;
     transition: var(--transition);
-    background: var(--main-bg);
+    background: var(--clr-main-bg);
     transform: translateX(-100%);
     z-index: -1;
+
+    @media (min-width: 992px) {
+      display: none;
+    }
   }
 
   .sidebar-show {
     transform: translateX(0);
     z-index: 999;
+  }
+
+  .sidebar__header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem 2.5rem 1rem 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  .sidebar__logo {
+    width: 40px;
+  }
+  .sidebar__btn-close {
+    display: flex;
+    align-items: center;
+
+    background: transparent;
+    border: none;
+    outline: none;
+    color: var(--clr-orange);
+    transition: var(--transition);
+    cursor: pointer;
+    font-size: 1.5rem;
+
+    &:hover {
+      transform: scale(0.9);
+    }
+  }
+
+  .sidebar__links {
+    padding: 1rem 2.5rem 1rem 1.5rem;
+  }
+
+  .sidebar__link {
+    margin-bottom: 1rem;
+    padding: 0;
+    display: flex;
+    align-items: center;
+
+    a {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 1.5rem;
+      text-transform: capitalize;
+      color: var(--clr-black);
+      transition: var(--transition);
+
+      position: relative;
+
+      &:hover {
+        transform: translateX(8px);
+      }
+    }
+
+    a.active::after {
+      content: '';
+      display: block;
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+      background: var(--clr-orange);
+      position: absolute;
+      right: -15px;
+      transform: translateY(25%);
+    }
+
+    svg {
+      margin-right: 0.4rem;
+      color: var(--clr-green-light-2);
+    }
+  }
+
+  .sidebar__users {
+    display: inline-block;
+    padding: 1rem 2.5rem 1rem 1.5rem;
   }
 `;
